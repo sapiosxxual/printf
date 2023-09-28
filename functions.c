6233dadb6608a5f1b5ecf212163140a4595e1c90
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-28 02:06:10 -0700

    printing functions

---
+++ functions.c
@@ -0,0 +1,138 @@
+#include "main.h"
+/**
+ * print_unsgnd - prints unsigned integer
+ * @n: the unsigned int
+ * Return: no of chars printed
+ */
+int print_unsgnd(unsigned int n)
+{
+	char buffer[1024];
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		buffer[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(buffer[i]);
+		i--;
+	}
+	return (i + 1);
+}
+/**
+ * print_octal - prints unsigned integer octal
+ * @n: the unsigned integer
+ * Return: mo of chars printed
+ */
+int print_octal(unsigned int n)
+{
+	char buffer[1024];
+	int i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		buffer[i] = (n % 8) + '0';
+		n /= 8;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(buffer[i]);
+		i--;
+	}
+	return (i + 1);
+}
+/**
+ * print_hex - prints unsigned int in hexadecimal
+ * @format: 'x' for lowercase, 'X' for uppercase
+ * @n: the unsigned integer
+ * Return: no of chars printe
+ */
+int print_hex(char format, unsigned int n)
+{
+	char buffer[1024];
+	int i = 0;
+	char hex_chars[] = "0123456789ABCDEF";
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		buffer[i] = hex_chars[n % 16];
+		n /= 16;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		if (format == 'x')
+			buffer[i] = buffer[i] >= 'A' && buffer[i] <= 'F' ? buffer[i] + 32
+				: buffer[i];
+		_putchar(buffer[i]);
+		i--;
+	}
+	return (i + 1);
+}
+/**
+ * print_S - prints a string with non printable char
+ * @str: string to be printed
+ * Return: no of chars printed
+ */
+int print_S(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count = print_hex('X', str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+/**
+ * print_pointer - print a memmory address in hexadecimal
+ * @n: the memory address to be printed
+ * Return: no of chars printed
+ */
+int print_pointer(unsigned int n)
+{
+	int count = 0;
+
+	_putchar('0');
+	_putchar('x');
+	count += 2;
+	count += print_hex('x', n);
+
+	return (count);
+}

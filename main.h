e0f90ed687bd08f09d0926a48271c9b163dd2b63
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-27 23:16:14 -0700

    header file for printf group project

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <atdarg.h>
+#include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _print_str(char *str);
+int _print_int(int n);
+int _print_binary(unsigned int n);
 #endif
